f9cd0a80c1d5f24b0b63b60f9ea606360ada9052
Jyotiborse23 <118126043+Jyotiborse23@users.noreply.github.com>
2023-01-04 23:26:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+var http=require('http');
+http.createServer(function(req, res)
+{
+    
+    res.end('hello i am learning noffejs');
+
+
+}).listen(8080);
+console.log('hello your server is getting reaaaady nowwwwwwwwwww')
